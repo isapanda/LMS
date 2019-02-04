ec63b3363b9a74f729def2987a5f2c89cc3b1cd9
isapanda <koro.koro.panda.dokoeiku@gmail.com>
2019-02-04 22:10:15 +0900

    4

--- Time&Temp.py
+++ Time&Temp.py
@@ -16,17 +16,11 @@ if(temp >= 4096):
  temp -= 819
 temp = temp * 0.0625
 
-#address = 0x50
-#block = i2c.read_i2c_block_data(address, 0x00, 1)
-#adc = block[0]
-
-#print("PSoC_DATA:%6.2f" % (temp / 16.0))
-
 d = datetime.datetime.today()
 dt = d.strftime("%Y%m%d")
 tm = d.strftime("%H%M%S")
-#csv = str(dt) + "," + str(tm) + "," + str(temp)  + ","+ str(adc) + ",\n"
-csv = str(dt) + "," + str(tm) + "," + str(temp) + ",\n"
 
-print csv
+disp = str(dt) + "," + str(tm) + "," + str(temp) + ",\n"
+
+print disp
 
